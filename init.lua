c65e07107d4d84031905414b9d092e5cb6ac83a7
Julian Pottle <julianpottle@gmail.com>
2021-09-27 12:44:07 -0400

    Added 'show current context' to indent_blankline

--- init.lua
+++ init.lua
@@ -165,6 +165,7 @@ vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_current_context = true
 
 -- CHADTree
 vim.api.nvim_set_keymap('n', '<leader>v', [[<cmd>CHADopen<CR>]], { noremap = true, silent = true })
