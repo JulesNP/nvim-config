032cc48f047e0ca338d83adeac54205c3b545f57
Julian Pottle <julianpottle@gmail.com>
2021-09-29 23:54:15 -0400

    Added firenvim

--- init.lua
+++ init.lua
@@ -52,6 +52,10 @@ require('packer').startup(function()
   use 'ms-jpq/chadtree' -- File Manager for Neovim, Better than NERDTree
   use 'justinmk/vim-sneak' -- The missing motion for Vim 👟
   use 'windwp/nvim-autopairs' -- A super powerful autopair for Neovim. It supports multiple characters.
+  use {
+      'glacambre/firenvim',
+      run = function() vim.fn['firenvim#install'](0) end
+  }
   -- use { 'ionide/Ionide-vim', run = 'make fsautocomplete' }
 end)
 
@@ -120,7 +124,9 @@ vim.o.termguicolors = true
 vim.g.onedark_terminal_italics = 2
 vim.g.gruvbox_italic = 1
 vim.cmd [[colorscheme gruvbox]]
-vim.cmd [[au VimEnter * highlight Normal guibg=0]]
+if not vim.g.started_by_firenvim then
+  vim.cmd [[au VimEnter * highlight Normal guibg=0]]
+end
 
 --Set statusbar
 require'lualine'.setup {
