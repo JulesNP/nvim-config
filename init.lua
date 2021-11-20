0f35b0663986a9d2dde602a23fd16623465b0845
Julian Pottle <julianpottle@gmail.com>
2021-11-20 09:15:42 -0500

    Updated lsp completion functions based on website documentation

--- init.lua
+++ init.lua
@@ -651,6 +651,11 @@ require("luasnip/loaders/from_vscode").lazy_load()
 local lspkind = require("lspkind")
 
 -- nvim-cmp setup
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require("cmp")
 cmp.setup({
   formatting = {
@@ -677,6 +682,8 @@ cmp.setup({
     ["<Tab>"] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
@@ -690,14 +697,14 @@ cmp.setup({
     end,
     ["<C-n>"] = function(fallback)
       if luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        luasnip.expand_or_jump()
       else
         fallback()
       end
     end,
     ["<C-p>"] = function(fallback)
       if luasnip.jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        luasnip.jump(-1)
       else
         fallback()
       end
