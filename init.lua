e67b4527f419c0d28021dd4df332d2093ec21cfb
Julian Pottle <julianpottle@gmail.com>
2021-12-04 01:03:55 -0500

    Changed diagnostics from eslint_d to eslint

--- init.lua
+++ init.lua
@@ -655,7 +655,7 @@ local null_ls = require("null-ls")
 null_ls.config({
   sources = {
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.stylua.with({
       extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
