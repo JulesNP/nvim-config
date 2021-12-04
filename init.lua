b0405a8e5a7a56825ba2b7f769f5827381bc745e
Julian Pottle <julianpottle@gmail.com>
2021-12-04 00:36:08 -0500

    has_words_before function no longer used

--- init.lua
+++ init.lua
@@ -656,12 +656,6 @@ require("luasnip/loaders/from_vscode").lazy_load()
 -- lspkind setup
 local lspkind = require("lspkind")
 
--- nvim-cmp setup
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local cmp = require("cmp")
 cmp.setup({
   experimental = {
